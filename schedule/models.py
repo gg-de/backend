f6f416f034606604dbd06c4010318380d817397c
Filipe Toyoshima <filipetoyoshima@gmail.com>
2021-01-30 18:33:41 -0300

    added app and migrated

--- schedule/models.py
+++ schedule/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.core.validators import MaxValueValiator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Subject(models.Model):
@@ -11,8 +11,8 @@ class Subject(models.Model):
 
 class Availability(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    weekday = models.IntegerField(validators=[MaxValueValiator(7), MinValueValidator(1)])
-    time = models.IntegerField(validators=[MaxValueValiator(0), MinValueValidator(23)])
+    weekday = models.IntegerField(validators=[MaxValueValidator(7), MinValueValidator(1)])
+    time = models.IntegerField(validators=[MaxValueValidator(0), MinValueValidator(23)])
 
 
 class Schedule(models.Model):
