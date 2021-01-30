c40c699e5d3286c9a64224d9ae80db6628d017d4
Filipe Toyoshima <filipetoyoshima@gmail.com>
2021-01-30 18:22:53 -0300

    Creating schedule models

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValiator, MinValueValidator
 
-# Create your models here.
+
+class Subject(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    week_hours = models.IntegerField(default=0)
+
+
+class Availability(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    weekday = models.IntegerField(validators=[MaxValueValiator(7), MinValueValidator(1)])
+    time = models.IntegerField(validators=[MaxValueValiator(0), MinValueValidator(23)])
+
+
+class Schedule(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    time = models.ForeignKey(Availability, on_delete=models.CASCADE)
